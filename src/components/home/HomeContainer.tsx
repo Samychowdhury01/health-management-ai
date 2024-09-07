9935ec187e1a79397ba1b54267f6f877da9f02d3
samychowdhury01 <samychowdhury84@gmail.com>
2024-09-07 20:33:24 +0600

    implemented support and features section in home page

--- src/components/home/HomeContainer.tsx
+++ src/components/home/HomeContainer.tsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import Support from "./Support";
 
 
 const HomeContainer = () => {
     return (
         <>
             <Banner/>
+            <Support/>
         </>
     );
 };
